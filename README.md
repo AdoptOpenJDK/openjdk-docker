74baa8f389081c20ce1487e787a085f3e2dcacd1
Dinakar Guniguntala <dinogun@gmail.com>
2017-10-12 18:12:50 +0530

    Multi-arch support for Docker images.

--- README.md
+++ README.md
@@ -1,11 +1,38 @@
 # OpenJDK and Docker
-Dockerfiles and build scripts for generating various Docker Images related to OpenJDK
+Dockerfiles and build scripts for generating various Docker Images related to OpenJDK. Currently this builds OpenJDK images with hotspot and Eclipse OpenJ9 on Ubuntu and Alpine Linux.
+
+# Supported Architectures
+Hotspot is supported on ```aarch64```, ```ppc64le```, ```s390x``` and ```x86_64```.
+Eclipse OpenJ9 is supported on ```ppc64le```, ```s390x``` and ```x86_64```.
 
 # License
 The Dockerfiles and associated scripts found in this project are licensed under the [Apache License 2.0.](http://www.apache.org/licenses/LICENSE-2.0.html).
 
-# How it works
+# Build and push the Images with multi-arch support
 
+```
+# Clone this github repo
+$ git clone https://github.com/AdoptOpenJDK/openjdk-docker
+
+# Build images and tag them appropriately
+$ cd openjdk-docker
+$ ./build_latest.sh
+
+# The above script generates a script (push_commands.sh) that has the right commands to push to hub.docker.com
+# Make sure to login to hub.docker first
+$ docker login
+$ ./push_commands.sh
+
+# build_latest.sh and push_commands.sh should be run on all supported architectures to build and push images to the docker registry. The images should now be available on hub.docker.com but without multi-arch support. To add multi-arch support, we need to generate the right manifest lists and push them to hub.docker.com. The script generate_manifest_script.sh can be used to generate the right manifest commands. This needs to be run only on x86_64 after docker images for all architecures have been built and made available on hub.docker.com
+$ ./generate_manifest_script.sh
+
+# generate_manifest_script.sh generates a script manifest-commands.sh, that creates the manifest list and pushes them to hub.docker.com. 
+$ ./manifest-commands.sh
+
+# We should now have the proper manifest lists pushed to hub.docker.com to support multi-arch pulls.
+```
+
+# Info on other scripts
 ```
 # Run ./generate_latest_sums.sh to get the shasums for the latest binaries available on adoptopenjdk.net
 ./generate_latest_sums.sh
@@ -13,9 +40,8 @@ The Dockerfiles and associated scripts found in this project are licensed under 
 # You should now have two files hotspot-shasums-latest.sh and openj9-shasums-latest.sh
 # These will have the shasums for the latest version for each of the supported arches for hotspot and Eclipse OpenJ9
 
-# You can now run update-multiarch.sh to generate the Dockerfiles for all supported arches for both
-# hotspot and Eclipse OpenJ9
+# You can now run update-multiarch.sh to generate the Dockerfiles for all supported arches for both hotspot and Eclipse OpenJ9.
 ./update-multiarch.sh
 
 # build_latest.sh will do all of the above and build the docker images for the current arch with the right set of tags
-./build_latest.sh
+```
---
+++ generate_manifest_script.sh
@@ -0,0 +1,230 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+set -e pipefail
+
+root_dir="$PWD"
+man_file=${root_dir}/manifest-commands.sh
+source_prefix="adoptopenjdk"
+source_repo="openjdk"
+
+# Where is the manifest tool installed?"
+# Manifest tool (docker with manifest support) needs to be added from here
+# https://github.com/clnperez/cli
+# $ cd /opt/manifest_tool
+# $ git clone -b manifest-cmd https://github.com/clnperez/cli.git
+# $ cd cli
+# $ make -f docker.Makefile cross
+manifest_tool_dir="/opt/manifest_tool"
+manifest_tool=${manifest_tool_dir}/cli/build/docker
+
+if [ ! -f ${manifest_tool} ]; then
+	echo
+	echo "ERROR: Docker with manifest support not found at path ${manifest_tool}"
+	exit 1
+fi
+
+# Remove any previously created manifest lists.
+# Currently there is no way to do this using the tool.
+rm -rf ~/.docker/manifests
+
+# Find the latest version and get the corresponding shasums
+./generate_latest_sums.sh
+
+source ./hotspot-shasums-latest.sh
+source ./openj9-shasums-latest.sh
+
+# Set the params based on the arch we are on currently
+machine=`uname -m`
+case $machine in
+aarch64)
+	arch="aarch64"
+	oses="ubuntu"
+	package="jdk"
+	version="9"
+	vms="hotspot"
+	;;
+ppc64le)
+	arch="ppc64le"
+	oses="ubuntu"
+	package="jdk"
+	version="9"
+	vms="hotspot openj9"
+	;;
+s390x)
+	arch="s390x"
+	oses="ubuntu"
+	package="jdk"
+	version="9"
+	vms="hotspot openj9"
+	;;
+x86_64)
+	arch="x86_64"
+	oses="ubuntu alpine"
+	package="jdk"
+	version="9"
+	vms="hotspot openj9"
+	;;
+*)
+	echo "ERROR: Unsupported arch:${machine}, Exiting"
+	exit 1
+	;;
+esac
+
+# Check if a given docker image exists on the server.
+# This script errors out if the image does not exist.
+function check_image() {
+	lrepo=$1
+	tag=$2
+
+	echo -n "INFO: Pulling image: ${lrepo}:${tag}..."
+	docker pull ${lrepo}:${tag} >/dev/null
+	if [ $? != 0 ]; then
+		echo "ERROR: Docker Image ${lrepo}:${tag} not found on hub.docker\n"
+		exit 1
+	fi
+	echo "done"
+}
+
+# Get the supported architectures for a given VM (Hotspot, OpenJ9).
+# This is based on the hotspot-shasums-latest.sh/openj9-shasums-latest.sh
+function get_arches() {
+	archsums="$(declare -p $1)";
+	eval "declare -A sums="${archsums#*=};
+	for arch in ${!sums[@]};
+	do
+		if [ "${arch}" != "version" ]; then
+			echo "${arch} "
+		fi
+	done
+}
+
+# build a set of valid docker image tags based on the VM and the supported arches.
+function build_tag_list() {
+	sums=$1
+	os=$2
+	rel=$3
+
+	if [ ${os} == "ubuntu" ]; then
+		supported_arches=$(get_arches ${sums})
+	else
+		supported_arches="x86_64"
+	fi
+	arch_tags=""
+	for sarch in ${supported_arches}
+	do
+		tag=${sarch}-${os}-${rel}
+		arch_tags="${arch_tags} ${tag}"
+	done
+	echo "${arch_tags}"
+}
+
+function print_annotate_cmd() {
+	main_tag=$1
+	arch_tag=$2
+
+	march=$(echo ${arch_tag} | awk -F':' '{ print $2 }' | awk -F'-' '{ print $1 }')
+	if [ ${march} == "x86_64" ]; then
+		march="amd64"
+	fi
+	echo "${manifest_tool} manifest annotate ${main_tag} ${arch_tag} --os linux --arch ${march}" >> ${man_file}
+}
+
+function print_manifest_cmd() {
+	release=$1
+	shift
+	arch_tags=$@
+
+	main_tags=""
+	os="$(echo ${arch_tags} | awk '{ print $1 }' | awk -F':' '{ print $2 }' | awk -F'-' '{ print $2 }')"
+	if [ ${os} == "ubuntu" ]; then
+		main_tags=${trepo}:$release
+		main_tags="${main_tags} ${trepo}:latest"
+	else
+		main_tags=${trepo}:$release-alpine
+	fi
+
+	for main_tag in ${main_tags}
+	do
+		echo "${manifest_tool} manifest create ${main_tag} ${arch_tags}" >> ${man_file}
+		declare -a tarr=( ${arch_tags} )
+		for i in `seq 0 $(( ${#tarr[@]} - 1 ))`
+		do
+			print_annotate_cmd ${main_tag} ${tarr[$i]}
+		done
+		echo "${manifest_tool} manifest push ${main_tag}" >> ${man_file}
+		echo >> ${man_file}
+	done
+}
+
+function print_tags() {
+	mantags="$(declare -p $1)";
+	eval "declare -A mtags="${mantags#*=};
+	for repo in ${!mtags[@]}
+	do
+		create_cmd=""
+		declare -a tarr=( ${mtags[$repo]} )
+		for i in `seq 0 $(( ${#tarr[@]} - 1 ))`
+		do
+			trepo=${source_prefix}/${repo}
+			check_image ${trepo} ${tarr[$i]}
+			create_cmd="${create_cmd} ${trepo}:${tarr[$i]}"
+		done
+		print_manifest_cmd ${rel} ${create_cmd}
+	done
+}
+
+# Valid image tags
+#adoptopenjdk/openjdk${version}:${arch}-${os}-${rel}
+#adoptopenjdk/openjdk${version}-openj9:${arch}-${os}-${rel}
+#
+declare -A manifest_tags_ubuntu
+declare -A manifest_tags_alpine
+for vm in ${vms}
+do
+	shasums="${package}"_"${vm}"_"${version}"_sums
+	jverinfo=${shasums}[version]
+	eval jrel=\${$jverinfo}
+	rel=$(echo $jrel | sed 's/+/./')
+
+	if [ "${vm}" == "hotspot" ]; then
+		srepo=${source_repo}${version}
+	else
+		srepo=${source_repo}${version}-${vm}
+	fi
+	for os in ${oses}
+	do
+		echo -n "INFO: Building tag list for [${vm}] and [${os}]..."
+		tag_list=$(build_tag_list ${shasums} ${os} ${rel})
+		if [ $os == "ubuntu" ]; then
+			manifest_tags_ubuntu[${srepo}]=${tag_list}
+		elif [ $os == "alpine" ]; then
+			manifest_tags_alpine[${srepo}]=${tag_list}
+		else
+			echo "ERROR: Unsupported OS: ${os}"
+			exit 1
+		fi
+		echo "done"
+	done
+done
+
+# Populate the script to create the manifest list
+echo "#!/bin/bash" > ${man_file}
+echo  >> ${man_file}
+
+print_tags manifest_tags_ubuntu
+print_tags manifest_tags_alpine
+
+chmod +x ${man_file}
+echo "INFO: Manifest commands in file: ${man_file}"
